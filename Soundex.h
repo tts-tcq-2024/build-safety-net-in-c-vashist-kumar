afcf66f02ed38c2f8b363b32814c9c277137d0a5
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-07-03 13:51:40 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -5,17 +5,56 @@
 #include <ctype.h>
 #include <string.h>
 
+char BFPV(char c){
+    if(c=='B' || c=='F' || c=='P' || c=='V'){
+        return '1';
+    }
+    return CGKKQSXZ(c);
+}
+
+char CGKKQSXZ(char c){
+    if(c=='C' || c=='G' || c=='J' || c=='K' || c=='Q' || c=='S' || c=='X' || c=='Z'){
+        return '2';
+    }
+    return  DT(c);
+}
+
+char DT(char c){
+    if(c=='D' || c=='T'){
+        return '3';
+    }
+    return L(c);
+}
+
+
+char L(char c){
+    if(c=='L'){
+        return'4';
+    }
+   return MN(c);
+}
+
+
+char MN(char c){
+    if(c=='N' || c=='M'){
+        return '5';
+    }
+    return R(c);
+}
+
+
+char R(char c){
+    if(c=='R'){
+        return '6';
+    }
+    return '0';
+}
+
+
 char getSoundexCode(char c) {
     c = toupper(c);
-    switch (c) {
-        case 'B': case 'F': case 'P': case 'V': return '1';
-        case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
-        case 'D': case 'T': return '3';
-        case 'L': return '4';
-        case 'M': case 'N': return '5';
-        case 'R': return '6';
-        default: return '0'; // For A, E, I, O, U, H, W, Y
-    }
+    return BFPV(c);
+    
 }
 
 void generateSoundex(const char *name, char *soundex) {
