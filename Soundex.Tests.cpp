3a33ce32cabfefd6b8a5d2a65137b35e2faa04b2
vashist-kumar <105800642+vashist-kumar@users.noreply.github.com>
2024-07-03 16:27:08 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -6,12 +6,7 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
   char soundex[5];
   generateSoundex("AX", soundex);
  //ASSERT_EQ(soundex,"A200");
- 
-}
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- //AAA
-  char soundex[5];
   generateSoundex("AT", soundex);
  //ASSERT_EQ(soundex,"A300");
- 
 }
+
